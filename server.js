3f66b71caa9726498808cd7890d08c2dbe764f00
Dhruv Ramdev <pythonbrilliant@gmail.com>
2018-06-02 01:10:49 +0530

    Added Cors

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import router from './routes/index';
 import mongodb from './database/mongodb';
+import cors from 'cors'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());
 app.use('/', router);
 
 mongodb.getConnection()
